acbbfd7d248eceb173eddeb721d9a2c7c1a6681b
Shu <shuyen.kuo1998@gmail.com>
2026-03-24 16:59:37 +0800

    cors&web update

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,17 +1,44 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    // 'paths' => ['api/*'],
+
+    // 'allowed_methods' => ['*'],
+
+    // 'allowed_origins' => [
+        // 'http://localhost:8100',
+        // 'http://127.0.0.1:8100',
+        // 'http://192.168.68.59:8100',
+        // 'https://gym-tracker-tawny.vercel.app', // 前端demo網址
+    // ],
+
+    // 'allowed_headers' => ['*'],
+
+    // 'supports_credentials' => true,
+
+
+    'paths' => [
+        'api/*', 
+        'sanctum/csrf-cookie'
+    ],
 
     'allowed_methods' => ['*'],
-
+    
     'allowed_origins' => [
         'http://localhost:8100',
         'http://127.0.0.1:8100',
         'http://192.168.68.59:8100',
-    ],
+        'https://gym-tracker-tawny.vercel.app', // 前端demo網址
+    ], 
 
+    
+    'allowed_origins_patterns' => [],
+    
     'allowed_headers' => ['*'],
-
-    'supports_credentials' => true,
+    
+    'exposed_headers' => [],
+    
+    'max_age' => 0,
+    
+    'supports_credentials' => true, // 如果有登入功能一定要設為 true
 ];
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,25 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/', function () {
-//     return 'Laravel OK';
-// });
-
-// Route::get('/', function () {
-//     return response()->json([
-//         'status' => 'ok',
-//         'env' => env('APP_ENV'),
-//         'db' => env('DB_CONNECTION')
-//     ]);
-// });
-
 Route::get('/', function () {
-    return [
-        'APP_KEY' => env('APP_KEY'),
-        'APP_ENV' => env('APP_ENV'),
-    ];
+    return view('welcome');
 });
+
+// Route::get('/', function () {
+//     return [
+//         'APP_KEY' => env('APP_KEY'),
+//         'APP_ENV' => env('APP_ENV'),
+//     ];
+// });
