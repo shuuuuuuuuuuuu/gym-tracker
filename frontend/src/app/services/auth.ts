9b76e50680052e078321f99bef301c02cbe8be58
Shu <shuyen.kuo1998@gmail.com>
2026-03-24 17:37:44 +0800

    url catch judge

--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -8,8 +8,12 @@ import { environment } from 'src/environments/environment';
 })
 export class AuthService {
 
-  private apiUrl = environment.apiUrl;
-
+  // private apiUrl = environment.apiUrl;
+  
+  // for demo
+  private apiUrl = window.location.hostname.includes('vercel.app') 
+  ? 'https://gym-tracker-gc9k.onrender.com/api' 
+  : 'http://127.0.0.1:8000/api';
 
   constructor(private http: HttpClient) {}
 
