35cd0263122c683da0903a20d657adadcf577dd5
Shu <shuyen.kuo1998@gmail.com>
2026-03-25 14:44:25 +0800

    local test ok

--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -8,14 +8,14 @@ import { environment } from 'src/environments/environment';
 })
 export class AuthService {
 
-  // private apiUrl = environment.apiUrl;
+  private apiUrl = environment.apiUrl;
   
   // // for demo
   // private apiUrl = window.location.hostname.includes('vercel.app') 
   // ? 'https://gym-tracker-gc9k.onrender.com/api' 
   // : 'http://127.0.0.1:8000/api';
 
-  private apiUrl = 'https://gym-tracker-gc9k.onrender.com/api';
+  // private apiUrl = 'https://gym-tracker-gc9k.onrender.com/api';
 
   constructor(private http: HttpClient) {}
 
