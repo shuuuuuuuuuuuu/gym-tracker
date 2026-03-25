0716395c0a0c016c2877a0e424080e297258372a
Shu <shuyen.kuo1998@gmail.com>
2026-03-25 17:13:46 +0800

    login style fix & config mode to ios

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -10,7 +10,11 @@ import { authInterceptor } from './interceptors/auth-interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
+    provideIonicAngular({
+      mode: 'ios',            // 強制全平台使用 iOS 視覺風格
+      // backButtonText: '返回',  // 順便統一返回按鈕文字
+      animated: true          // 確保轉場動畫開啟
+    }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(
       withInterceptors([authInterceptor])
