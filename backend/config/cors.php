7dacd6ca1a640ba19814d4f889b50bb9a736e655
Shu <shuyen.kuo1998@gmail.com>
2026-03-28 15:53:27 +0800

    cors setting fix

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,22 +1,6 @@
 <?php
 
 return [
-    // 'paths' => ['api/*'],
-
-    // 'allowed_methods' => ['*'],
-
-    // 'allowed_origins' => [
-        // 'http://localhost:8100',
-        // 'http://127.0.0.1:8100',
-        // 'http://192.168.68.59:8100',
-        // 'https://gym-tracker-tawny.vercel.app', // 前端demo網址
-    // ],
-
-    // 'allowed_headers' => ['*'],
-
-    // 'supports_credentials' => true,
-
-
     'paths' => [
         'api/*', 
         'sanctum/csrf-cookie'
@@ -28,7 +12,7 @@ return [
         'http://localhost:8100',
         'http://127.0.0.1:8100',
         'http://192.168.68.59:8100',
-        'https://gym-tracker-tawny.vercel.app', // 前端demo網址
+        'https://gym-tracker-gc7e6hfpc-shuyenkuo1998-7925s-projects.vercel.app', // 前端demo網址
     ], 
 
     
